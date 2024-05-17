866fce5f9488fa64f18a20d31af77fcdb141c0da
mimo626 <yessuk5499@gmail.com>
2024-05-17 10:38:34 +0900

    [feat] home_screen의 UserStroy 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,8 +5,62 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [const StoryArea(), FeedList()],
+      ),
     );
   }
 }
+
+class StoryArea extends StatelessWidget {
+  const StoryArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+            10, (index) => UserStory(userName: "user $index")),
+      ),
+    );
+  }
+}
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+
+  const UserStory({required this.userName, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8.0),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('lib/assets/images/profile.PNG')),
+                borderRadius: BorderRadius.circular(40)),
+          ),
+          Text(userName)
+        ],
+      ),
+    );
+  }
+}
+
