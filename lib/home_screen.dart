905da644c6cbcb388ff7a4c7c72a57da0070100b
mimo626 <yessuk5499@gmail.com>
2024-05-17 11:04:01 +0900

    [feat] FeedList 프로필

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,8 +21,8 @@ class StoryArea extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: List.generate(
-            10, (index) => UserStory(userName: "user $index")),
+        children:
+            List.generate(10, (index) => UserStory(userName: "user $index")),
       ),
     );
   }
@@ -55,25 +55,26 @@ class UserStory extends StatelessWidget {
   }
 }
 
-class FeedData{
+class FeedData {
   final String userName;
   final int likeCount;
   final String content;
 
-  FeedData({required this.userName, required this.likeCount, required this.content});
-
+  FeedData(
+      {required this.userName, required this.likeCount, required this.content});
 }
+
 final feedDataList = [
-FeedData(userName: "User1", likeCount: 50, content: "오늘 점심 맛있다."),
-FeedData(userName: "User2", likeCount: 22, content: "오늘 아침 맛있다."),
-FeedData(userName: "User3", likeCount: 33, content: "저녁 뭐 먹지."),
-FeedData(userName: "User4", likeCount: 43, content: "고양이 귀엽다."),
-FeedData(userName: "User5", likeCount: 12, content: "다들 뭐해?"),
-FeedData(userName: "User6", likeCount: 56, content: "오늘 저녁 맛있다."),
-FeedData(userName: "User7", likeCount: 60, content: "어제 점심 맛있다."),
-FeedData(userName: "User8", likeCount: 75, content: "어제 아침 맛있다."),
-FeedData(userName: "User9", likeCount: 34, content: "어제 저녁 맛있다."),
-FeedData(userName: "User10", likeCount: 39, content: "오늘 점심 맛없다."),
+  FeedData(userName: "User1", likeCount: 50, content: "오늘 점심 맛있다."),
+  FeedData(userName: "User2", likeCount: 22, content: "오늘 아침 맛있다."),
+  FeedData(userName: "User3", likeCount: 33, content: "저녁 뭐 먹지."),
+  FeedData(userName: "User4", likeCount: 43, content: "고양이 귀엽다."),
+  FeedData(userName: "User5", likeCount: 12, content: "다들 뭐해?"),
+  FeedData(userName: "User6", likeCount: 56, content: "오늘 저녁 맛있다."),
+  FeedData(userName: "User7", likeCount: 60, content: "어제 점심 맛있다."),
+  FeedData(userName: "User8", likeCount: 75, content: "어제 아침 맛있다."),
+  FeedData(userName: "User9", likeCount: 34, content: "어제 저녁 맛있다."),
+  FeedData(userName: "User10", likeCount: 39, content: "오늘 점심 맛없다."),
 ];
 
 class FeedList extends StatelessWidget {
@@ -88,28 +89,44 @@ class FeedList extends StatelessWidget {
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
       itemCount: feedDataList.length,
-        itemBuilder: (context, index) => FeedItem(feedData: feedDataList[index]),
+      itemBuilder: (context, index) => FeedItem(feedData: feedDataList[index]),
     );
   }
 }
 
 class FeedItem extends StatelessWidget {
   final FeedData feedData;
+
   const FeedItem({required this.feedData, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-
-          ],
-        )
+        Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              image: AssetImage('lib/assets/images/pure.PNG'))),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(feedData.userName),
+                  ],
+                ),
+                const Icon(Icons.more_vert)
+              ],
+            )
+        ),
       ],
     );
   }
 }
-
-
-
